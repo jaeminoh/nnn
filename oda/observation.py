ca41d76d26b251983a31f87ad9711ba8475548a7
jaeminoh <jaeminoh.math@gmail.com>
2024-12-04 19:19:19 +0900

    add subsampling observation operator

---
+++ oda/observation.py
@@ -0,0 +1,21 @@
+from beartype import beartype as typechecker
+from jaxtyping import ArrayLike, Float, jaxtyped
+
+
+class ObservationOperator:
+    pass
+
+
+class UniformSubsample(ObservationOperator):
+    def __init__(self, num_spatial_dims: int = 1, sensor_every: int = 1):
+        self.num_spatial_dims=num_spatial_dims
+        self.sensor_every = sensor_every
+
+    @jaxtyped(typechecker=typechecker)
+    def __call__(
+        self, x: Float[ArrayLike, "..."]
+    ) -> Float[ArrayLike, "..."]:
+        if self.num_spatial_dims == 1:
+            return x[:: self.sensor_every]
+        elif self.num_spatial_dims == 2:
+            return x[:: self.sensor_every, :: self.sensor_every]
