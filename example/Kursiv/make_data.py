6f9e7dc29190e4527231bb9f1dbdd929f4bfc239
jaeminoh <jaeminoh.math@gmail.com>
2025-04-18 15:20:16 -0500

    match temporal integrators

--- example/Kursiv/make_data.py
+++ example/Kursiv/make_data.py
@@ -1,15 +1,18 @@
 import os
 
+import jax
 import numpy as np
-from numpy.fft import irfft, rfft, rfftfreq
 from tqdm import trange
+
+from oda.models import Kursiv
 
 
 def kursiv(Nx: int = 128, M: int = 16, draw_plot: bool = False):
     if not os.path.isdir("data"):
         os.mkdir("data")
 
-    print(f"Precision check: {np.ones(()).dtype}")
+    jax.config.update("jax_enable_x64", True)
+    print(f"Precision check: {jax.numpy.ones(()).dtype}")
 
     """
     A numerical solver of Kuramoto-Sivashinsky equation by ETDRK4 scheme
@@ -17,47 +20,22 @@ def kursiv(Nx: int = 128, M: int = 16, draw_plot: bool = False):
     # spatial grid and initial condition
     xx = np.linspace(0, 32 * np.pi, Nx + 1)[:-1]
     u = np.cos(xx / 16) * (1 + np.sin(xx / 16))
-    v = rfft(u)
+    model = Kursiv(Nx=Nx, dt=1 / 4)
 
-    # precompute quantities
     t = 0.0
-    h = 1 / 4
-    k = 2j * np.pi * rfftfreq(Nx, 32 * np.pi / Nx)
-    L = -(k**2) - k**4
-    E = np.exp(h * L)
-    E2 = np.exp(h * L / 2)
-    M = 16  # the number of quadrature points for the contour integral
-    r = np.exp(1j * np.pi * np.arange(1 - 0.5, M + 1 - 0.5) / M)
-    LR = h * L[:, None] + r
-    Q = h * ((np.exp(LR / 2) - 1) / LR).mean(1).real
-    f1 = h * ((-4 - LR + np.exp(LR) * (4 - 3 * LR + LR**2)) / LR**3).mean(1).real
-    f2 = h * ((2 + LR + np.exp(LR) * (-2 + LR)) / LR**3).mean(1).real
-    f3 = h * ((-4 - 3 * LR - LR**2 + np.exp(LR) * (4 - LR)) / LR**3).mean(1).real
 
-    vv = [v]
+    uu = [u]
     tt = [0.0]
     tmax = 10000
-    nmax = int(tmax // h)
-
-    def step(t, v):
-        t = t + h
-        Nv = -0.5 * k * rfft(irfft(v, Nx) ** 2)
-        a = E2 * v + Q * Nv
-        Na = -0.5 * k * rfft(irfft(a, Nx) ** 2)
-        b = E2 * v + Q * Na
-        Nb = -0.5 * k * rfft(irfft(b, Nx) ** 2)
-        c = E2 * a + Q * (2 * Nb - Nv)
-        Nc = -0.5 * k * rfft(irfft(c, Nx) ** 2)
-        v = E * v + Nv * f1 + 2 * (Na + Nb) * f2 + Nc * f3
-        return t, v
+    nmax = int(tmax // model.dt)
 
     for _ in trange(nmax):
-        t, v = step(t, v)
-        tt.append(t), vv.append(v)
+        t += model.dt
+        u = model._step(u)
+        tt.append(t), uu.append(u)
 
     tt = np.stack(tt)
-    vv = np.stack(vv)
-    uu = irfft(vv, Nx, axis=-1)
+    uu = np.stack(uu)
 
     np.savez("data/train.npz", tt=tt[5000:-25000], sol=uu[5000:-25000])
     np.savez("data/test.npz", tt=tt[15000:], sol=uu[15000:])
--- oda/models.py
+++ oda/models.py
@@ -1,6 +1,9 @@
+import functools
+
 import jax
 import jax.numpy as jnp
 import numpy as np
+from jax.numpy.fft import rfftfreq, rfft, irfft
 
 from oda.observation import UniformSubsample
 
@@ -27,6 +30,7 @@ class DynamicalCore:
     def __call__(self):
         raise NotImplementedError
 
+    @functools.partial(jax.jit, static_argnums=0)
     def _step(self):
         raise NotImplementedError
 
@@ -69,7 +73,7 @@ class Lorenz96(DynamicalCore):
         )
 
     def _step(self, u0):
-        return u0 + self.dt * self(u0)
+        return _rk4(self, u0, self.dt)
 
 
 class Kursiv(DynamicalCore):
@@ -90,6 +94,7 @@ class Kursiv(DynamicalCore):
     ):
         super().__init__(Nx=Nx, dt=dt, inner_steps=inner_steps, **kwargs)
         self.k = 2j * np.pi * jnp.fft.rfftfreq(self.Nx, (xr - xl) / self.Nx)
+        self._etdrk4_precompute()
 
     def __call__(self, u):
         linear = jnp.fft.irfft((-(self.k**2) - self.k**4) * jnp.fft.rfft(u), self.Nx)
@@ -97,4 +102,50 @@ class Kursiv(DynamicalCore):
         return linear + nonlinear
 
     def _step(self, u0):
-        return u0 + self.dt * self(u0)
+        # u0 + self.dt * self(u0) forward euler
+        return irfft(self._etdrk4(rfft(u0)), self.Nx)
+
+    def _etdrk4_precompute(self):
+        h = self.dt
+        k = 2j * np.pi * rfftfreq(self.Nx, 32 * np.pi / self.Nx)
+        L = -(k**2) - k**4
+        E = np.exp(h * L)
+        E2 = np.exp(h * L / 2)
+        M = 16  # the number of quadrature points for the contour integral
+        r = np.exp(1j * np.pi * np.arange(1 - 0.5, M + 1 - 0.5) / M)
+        LR = h * L[:, None] + r
+        Q = h * ((np.exp(LR / 2) - 1) / LR).mean(1).real
+        f1 = h * ((-4 - LR + np.exp(LR) * (4 - 3 * LR + LR**2)) / LR**3).mean(1).real
+        f2 = h * ((2 + LR + np.exp(LR) * (-2 + LR)) / LR**3).mean(1).real
+        f3 = h * ((-4 - 3 * LR - LR**2 + np.exp(LR) * (4 - LR)) / LR**3).mean(1).real
+
+        self.k = k
+        self.E = E
+        self.E2 = E2
+        self.Q = Q
+        self.f1 = f1
+        self.f2 = f2
+        self.f3 = f3
+
+    def _etdrk4(self, v):
+        Nv = -0.5 * self.k * rfft(irfft(v, self.Nx) ** 2)
+        a = self.E2 * v + self.Q * Nv
+        Na = -0.5 * self.k * rfft(irfft(a, self.Nx) ** 2)
+        b = self.E2 * v + self.Q * Na
+        Nb = -0.5 * self.k * rfft(irfft(b, self.Nx) ** 2)
+        c = self.E2 * a + self.Q * (2 * Nb - Nv)
+        Nc = -0.5 * self.k * rfft(irfft(c, self.Nx) ** 2)
+        v = self.E * v + Nv * self.f1 + 2 * (Na + Nb) * self.f2 + Nc * self.f3
+        return v
+
+
+def _rk4(f: callable, u0, h: float):
+    k1 = f(u0)
+    k2 = f(u0 + k1 * h / 2)
+    k3 = f(u0 + k2 * h / 2)
+    k4 = f(u0 + k3 * h)
+    return u0 + (k1 + 2 * k2 + 2 * k3 + k4) * h / 6
+
+
+def _forward_euler(f: callable, u0, h: float):
+    return u0 + h * f(u0)
